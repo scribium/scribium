f5a70bdbeafbe0f58aacdd83497f46fb1a0ce9e4
Adrian Polak <adrian.polak20@gmail.com>
2022-11-10 13:03:18 -0800

    feat(api): add prisma (#3)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-	imports: [],
+	imports: [PrismaModule],
 })
 export class AppModule {}
---
+++ apps/api/src/prisma/prisma.module.ts
@@ -0,0 +1,20 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+export const PRISMA_TOKEN = Symbol('PRISMA_TOKEN');
+
+@Global()
+@Module({
+	providers: [
+		{
+			provide: PRISMA_TOKEN,
+			useFactory: async () => {
+				const client = new PrismaClient();
+
+				await client.$connect();
+				return client;
+			},
+		},
+	],
+})
+export class PrismaModule {}
