4fb93a81532017f7e03570a6be4c6a7f6f004bd4
adipol1359 <adrian.polak20@gmail.com>
2022-11-11 13:29:22 +0100

    refactor(api): change imports to types

--- apps/api/src/users/users.mapper.ts
+++ apps/api/src/users/users.mapper.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { UserDto } from './dto/user.dto';
-import { AppUser } from './interfaces/app-user.interface';
+
+import type { UserDto } from './dto/user.dto';
+import type { AppUser } from './interfaces/app-user.interface';
 
 @Injectable()
 export class UsersMapper {
