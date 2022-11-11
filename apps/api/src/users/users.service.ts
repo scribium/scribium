970705d4ffc34d9f6e2d58802a932b5222f60a79
adipol1359 <adrian.polak20@gmail.com>
2022-11-11 12:38:04 +0100

    feat(api): set salt or rounds from env

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -4,15 +4,18 @@ import { Inject, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PRISMA_TOKEN } from 'src/prisma/prisma.module';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
 
 import type { AppUser } from './interfaces/app-user.interface';
+import type { AppConfigService } from 'src/app.types';
 
 const include = { details: true } as const;
 
 @Injectable()
 export class UsersService {
 	constructor(
-		@Inject(PRISMA_TOKEN) private readonly prismaClient: PrismaClient
+		@Inject(PRISMA_TOKEN) private readonly prismaClient: PrismaClient,
+		@Inject(ConfigService) private readonly configService: AppConfigService
 	) {}
 
 	async createUser({
@@ -42,6 +45,9 @@ export class UsersService {
 	}
 
 	private hashPassword(password: string): Promise<string> {
-		return bcrypt.hash(password, 10); // TODO: SET ROUNDS FROM CONFIG
+		return bcrypt.hash(
+			password,
+			this.configService.get('SALT_OR_ROUNDS', { infer: true })
+		);
 	}
 }
