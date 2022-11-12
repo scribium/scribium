0b2683ea24221baf364b8b63d4b6124ee9924875
adipol1359 <adrian.polak20@gmail.com>
2022-11-12 13:09:50 +0100

    feat(web): add font

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-poppins)', ...fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
