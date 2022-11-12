be3951bc23e6f355ace4ebb954ca16fc1245c669
Adrian Polak <adrian.polak20@gmail.com>
2022-11-12 07:09:27 -0800

    feat/main-layout (#6)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,18 @@
-import './global.css';
+import { Poppins } from '@next/font/google';
 
 import type { ReactNode } from 'react';
 
+import './globals.css';
+
+const poppins = Poppins({
+	weight: ['400', '500', '600'],
+	subsets: ['latin'],
+	variable: '--font-poppins',
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={poppins.variable}>
 			<head />
 			<body>{children}</body>
 		</html>
