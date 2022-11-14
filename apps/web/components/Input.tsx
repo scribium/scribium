9b51a8aa5e67e52d0961daf79681f7d098897103
adipol1359 <adrian.polak20@gmail.com>
2022-11-14 09:09:45 +0100

    feat(web): add forward ref

--- apps/web/components/Input.tsx
+++ apps/web/components/Input.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react';
+import { forwardRef, useId } from 'react';
 
 import type { InputHTMLAttributes } from 'react';
 
@@ -9,39 +9,38 @@ type Props = Readonly<{
 }> &
 	InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = ({
-	label,
-	helperText,
-	className,
-	error,
-	...rest
-}: Props) => {
-	const id = useId();
+export const Input = forwardRef<HTMLInputElement, Props>(
+	({ label, helperText, className, error, ...rest }, ref) => {
+		const id = useId();
 
-	return (
-		<div className={`flex flex-col ${className}`}>
-			<label
-				className="mb-2 tracking-wide text-gray-700 text-xs font-bold uppercase"
-				htmlFor={id}
-			>
-				{label}
-			</label>
-			<input
-				id={id}
-				className={`appearance-none bg-gray-200 text-gray-700 border rounded py-2.5 px-4 focus:outline-none focus:bg-white focus:border-gray-500 ${
-					error ? 'border-red-500' : 'border-gray-200'
-				}`}
-				{...rest}
-			/>
-			{helperText && (
-				<p
-					className={`text-xs italic mt-3 ${
-						error ? 'text-red-500' : 'text-gray-600'
-					}`}
+		return (
+			<div className={`flex flex-col ${className}`}>
+				<label
+					className="mb-2 tracking-wide text-gray-700 text-xs font-bold uppercase"
+					htmlFor={id}
 				>
-					{helperText}
-				</p>
-			)}
-		</div>
-	);
-};
+					{label}
+				</label>
+				<input
+					id={id}
+					className={`appearance-none bg-gray-200 text-gray-700 border rounded py-2.5 px-4 focus:outline-none focus:bg-white focus:border-gray-500 ${
+						error ? 'border-red-500' : 'border-gray-200'
+					}`}
+					ref={ref}
+					{...rest}
+				/>
+				{helperText && (
+					<p
+						className={`text-xs italic mt-3 ${
+							error ? 'text-red-500' : 'text-gray-600'
+						}`}
+					>
+						{helperText}
+					</p>
+				)}
+			</div>
+		);
+	}
+);
+
+Input.displayName = 'Input';
