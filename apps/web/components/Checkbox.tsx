cb4f57c2252a4bf2941a7d021345161912ab297c
adipol1359 <adrian.polak20@gmail.com>
2022-11-14 11:10:24 +0100

    feat(web): add checkbox error prop

--- apps/web/components/Checkbox.tsx
+++ apps/web/components/Checkbox.tsx
@@ -1,23 +1,32 @@
 import { forwardRef } from 'react';
 
 import type { InputHTMLAttributes, ReactNode } from 'react';
+import type { FieldError } from 'react-hook-form';
 
 type Props = Readonly<{
 	label: ReactNode;
+	error?: FieldError;
 }> &
 	InputHTMLAttributes<HTMLInputElement>;
 
 export const Checkbox = forwardRef<HTMLInputElement, Props>(
-	({ label, ...rest }, ref) => (
-		<label className="flex items-center text-gray-800">
-			<input
-				type="checkbox"
-				className="accent-primary mr-1.5"
-				ref={ref}
-				{...rest}
-			/>{' '}
-			{label}
-		</label>
+	({ label, error, ...rest }, ref) => (
+		<div>
+			<label className="flex items-center text-gray-800">
+				<input
+					type="checkbox"
+					className="accent-primary mr-1.5"
+					ref={ref}
+					{...rest}
+				/>{' '}
+				{label}
+			</label>
+			{error && (
+				<span className="text-xs italic mt-3 text-red-500">
+					{error.message}
+				</span>
+			)}
+		</div>
 	)
 );
 
